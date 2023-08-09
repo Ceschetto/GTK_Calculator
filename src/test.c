d519c1ad9f75430b3f1fc5756e552103327112bc
Ceschetto <francesco.bocci09@gmail.com>
2023-08-09 09:42:20 +0200

    Ho aggiunto tutti i bottoni tranne quello per creare numeri frazionari. Per poter completare la calcolatrice bisogna ancora migliorarne la logica gestendo meglio l'input degli operandi. Attualmente è ad un livello che definire elementare è eccessivo.

--- src/test.c
+++ src/test.c
@@ -4,7 +4,7 @@
 
 void print_results(void)
 {
-  g_print("%lf", get_result());
+  g_print("%0.2lf\n", get_result());
 }
 
 
@@ -29,7 +29,7 @@ void activate(GtkApplication *appPtr, gpointer data)
   for(int i = 9, j= 0; i >= 0; i--)
   {
     if(i != 9 && (i%3 == 0)) j++;
-    char stri[] = {i+48}; //ascii table trick
+    char stri[] = {i+48, '\0'}; //ascii table trick
     button = gtk_button_new_with_label(stri);
     g_signal_connect_swapped(button, "clicked", G_CALLBACK(set_operand), i ); //da capire sta storia del gpointer data come parametro
     gtk_grid_attach(GTK_GRID( grid ), button, i%3, j, 1, 1);
@@ -38,12 +38,16 @@ void activate(GtkApplication *appPtr, gpointer data)
 
   for(int i = 0; i<4; i++)
   {
-    char stri[] = { (i<2)? i+42 : 2*i + 41 }; //ascii table trick
-    button = gtk_button_new_with_label(stri);
-    g_signal_connect_swapped(button, "clicked", G_CALLBACK(set_operation), i ); //char e int sono compatibili
+    char operatorStr[] = { (i<2)? i+42 : 2*i + 41, '\0'  }; //ascii parkour
+    button = gtk_button_new_with_label(operatorStr);
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK(set_operation), operatorStr[0] ); //char e int sono compatibili
     gtk_grid_attach(GTK_GRID( grid ), button, 3, i, 1, 1);
-     
   }
+
+  button = gtk_button_new_with_label("=");
+  g_signal_connect(button, "clicked", G_CALLBACK(print_results), NULL);
+  gtk_grid_attach( GTK_GRID( grid ), button, 2, 3, 1, 1 );
+
  
 
   gtk_widget_set_visible(window, true);
